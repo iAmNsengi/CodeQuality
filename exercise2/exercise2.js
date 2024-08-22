9760cb25a19dbe1a736e6e26dd85389bf8410bb3
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-22 08:59:59 +0200

    fix: greetuser

--- exercise2/exercise2.js
+++ exercise2/exercise2.js
@@ -1,7 +1,4 @@
-function greet(name) {
-  if (name) {
-    return "Hello, " + name + "!";
-  } else {
-    return "Hello!";
-  }
+function greetUser(name) {
+  // checking if the name exists and retirn hello + name ! or return Hello!
+  return name ? "Hello, " + name + "!" : "Hello!";
 }
