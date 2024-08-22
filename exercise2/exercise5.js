09443f9de6fdd59429e70a6d97c330910345da2a
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-22 09:04:39 +0200

    fix: factorial

--- exercise2/exercise5.js
+++ exercise2/exercise5.js
@@ -1,11 +1,4 @@
 function calculateFactorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    var factorial = 1;
-    for (var i = 2; i <= n; i++) {
-      factorial *= i;
-    }
-    return factorial;
-  }
+  // using recursive method
+  return n === 0 || n === 1 ? 1 : n * calculateFactorial(n - 1);
 }
