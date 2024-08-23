697e739a2e9c9bd428662d81c9d47cacebb93853
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-23 12:03:31 +0200

    fix: exercise 2

--- exercise3/exercise2.js
+++ exercise3/exercise2.js
@@ -1,1 +1,11 @@
 // Write a JavaScript function to check if a given number is prime or not.
+const checkPrime = (num) => {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  // Create an array from 2 to the square root of num
+  const factors = Array.from(
+    { length: Math.floor(Math.sqrt(num)) - 1 },
+    (el, i) => i + 2
+  );
+  return factors.every((i) => num % i !== 0);
+};
