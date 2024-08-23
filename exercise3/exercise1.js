925a486fbe279309c62084954b67b8d8e9f027bb
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-23 10:05:43 +0200

    fix: exercise 1

--- exercise3/exercise1.js
+++ exercise3/exercise1.js
@@ -1,2 +1,6 @@
 // Write a function(s) that checks the level of nesting of an array.
 // Then, use that function to flatten the array into a single - dimensional array.
+const getNestingLevel = (arr) =>
+  !Array.isArray(arr) ? 0 : 1 + Math.max(0, ...arr.map(getNestingLevel));
+
+const flattenArray = (arr) => arr.flat(Infinity);
